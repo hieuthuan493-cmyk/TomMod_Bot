f1da8fccf44916b622d65e65857d8856c7be103f
hieuthuan493-cmyk <hieuthuan493@gmail.com>
2026-09-16 18:07:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Lấy Token từ biến môi trường trên Railway (hoặc thay trực tiếp token bot của mày vào đây)
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8882626353:AAFlup4z-OxSQL-oaeTqndDHkn238WcTrEI")
+TOKEN = os.getenv("TOKEN", "8882626353:AAFlup4z-OxSQL-oaeTqndDHkn238WcTrEI")
 
 # Danh sách 100 câu chửi tục / câu spam vô hạn
 spam_texts = [
